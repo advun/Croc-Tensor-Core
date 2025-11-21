166ea216ab5dae56bbe71c76d8afd1c21405b6d6
advun <186046511+advun@users.noreply.github.com>
2025-11-21 01:52:41 -0500

    first write

---
+++ state_machine.sv
@@ -0,0 +1,34 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 11/21/2025 01:30:12 AM
+// Design Name: 
+// Module Name: state_machine
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module state_machine import matrix_pkg::indata_size;(
+    input wire [31:0] cntrl,
+    input wire [31:0] fetch,
+    input wire reset,
+    input wire clk,
+    output wire signed [indata_size-1:0] a1X, 
+    output wire signed [indata_size-1:0] a2X,
+    output wire signed [indata_size-1:0] bX1,
+    output wire signed [indata_size-1:0] bX2
+    );
+    
+endmodule
