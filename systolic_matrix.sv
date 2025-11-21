1356f19fefc217764b716f2a042eca99e584cee1
advun <cameronpwilding@gmail.com>
2025-11-21 16:08:04 -0500

    Update systolic_matrix.sv
    Added pushes

--- systolic_matrix.sv
+++ systolic_matrix.sv
@@ -23,6 +23,9 @@
 module systolic_matrix import matrix_pkg::indata_size;(
     input wire clk,
     input wire reset,
+    input wire push11,
+    input wire pushedge,
+    input wire push22,
     input wire signed [indata_size-1:0] a1X, 
     input wire signed [indata_size-1:0] a2X,
     input wire signed [indata_size-1:0] bX1,
@@ -40,6 +43,7 @@ module systolic_matrix import matrix_pkg::indata_size;(
     processing_element p11 (
     .clk(clk),
     .reset(reset),
+    .push(push11),
     .in_a(a1X),
     .in_b(bX1),
     .out_c(c11),
@@ -50,6 +54,7 @@ module systolic_matrix import matrix_pkg::indata_size;(
     processing_element_rightedge p12 (
     .clk(clk),
     .reset(reset),
+    .push(pushedge),
     .in_a(p11_aout),
     .in_b(bX2),
     .out_c(c12),
@@ -59,6 +64,7 @@ module systolic_matrix import matrix_pkg::indata_size;(
     processing_element_bottomedge p21 (
     .clk(clk),
     .reset(reset),
+    .push(pushedge),
     .in_a(a2X),
     .in_b(p11_bout),
     .out_c(c21),
@@ -68,6 +74,7 @@ module systolic_matrix import matrix_pkg::indata_size;(
     processing_element_rightbottomcorner p22 (
     .clk(clk),
     .reset(reset),
+    .push(push22),
     .in_a(p21_aout),
     .in_b(p12_bout),
     .out_c(c22)
