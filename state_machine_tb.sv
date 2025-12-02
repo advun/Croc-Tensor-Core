e696f9912c8965c9b9119d2e09de85ca968cdf34
advun <186046511+advun@users.noreply.github.com>
2025-12-02 02:53:50 -0500

    final?

--- state_machine_tb.sv
+++ state_machine_tb.sv
@@ -1,29 +1,12 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/01/2025 08:05:55 PM
-// Design Name: 
-// Module Name: state_machine_tb
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
+
 
 
 module state_machine_tb(
 
     );
     
+    localparam SIZER = 4; //ONLY EVEN NUMBERS
     
     reg [31:0] inA;
     reg [31:0] inB;
@@ -39,6 +22,7 @@ module state_machine_tb(
     wire signed [7:0] a2X;
     wire signed [7:0] bX1;
     wire signed [7:0] bX2;
+    wire valid;
     
     
     state_machine s0(
@@ -46,6 +30,7 @@ module state_machine_tb(
     .inB(inB),
     .size(size),
     .reset(reset),
+    .valid(valid),
     .clk(clk),
     .start(start),
     .push11(push11),
@@ -57,6 +42,11 @@ module state_machine_tb(
     .bX2(bX2)
     );
     
+    reg signed [31:0] A [((SIZER/2)-1):0][((SIZER/2)-1):0]; //[i] = row [j] = column
+    reg signed [31:0] B [((SIZER/2)-1):0][((SIZER/2)-1):0];
+    
+    
+    
     initial clk = 1;
     always #25 clk = ~clk; // 50 nsec clock = 20Mhz
     
@@ -64,15 +54,42 @@ module state_machine_tb(
     reset <= 1;
     inA <= 0;
     inB <= 0;
-    size <= 17'd4;
+    size <= SIZER;
     start <= 0;
-    #50;
+    A[0][0] <= 32'haabbccdd;
+    A[1][0] <= 32'hbacadaee;
+    A[0][1] <= 32'hafbdcada;
+    A[1][1] <= 32'hbecedeed;
     
+    B[0][0] <= 32'hfaeadaca;
+    B[1][0] <= 32'hebdbcbba;
+    B[0][1] <= 32'hffeeddcc;
+    B[1][1] <= 32'heeddccbb;
+    
+    #50;
     reset <= 0;
     start <= 1;
+    #50;
+    start <= 0;
     
+    // Stream in matrixes
+    for (int i = 0; i < (SIZER/2); i++) begin      // For each output row block
+        for (int j = 0; j < (SIZER/2); j++) begin  // For each output column block
+            for (int k = 0; k < (SIZER/2); k++) begin  // For each block in the dot product
+                inA <= A[i][k];
+                #50;
+                inB <= B[k][j];
+                #50;
+            end
+        end
+    end
+ 
+    inA <= 0;
+    inB <= 0;
     
+    #10000;
     $finish;
+
     end
     
 endmodule
